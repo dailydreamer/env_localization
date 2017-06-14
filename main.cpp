7e963d924deaaad9c2b8fef2ed92ebe231f63d5c
dailydreamer <lixc12@outlook.com>
2017-06-14 17:01:56 +0800

    add extrinsic parameter calib

--- main.cpp
+++ main.cpp
@@ -3,13 +3,16 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/bgsegm.hpp>
+#include <opencv2/calib3d.hpp>
 #include "ipm.h"
 
 void subtractBackground(std::string videoPath, bool debug = false) {
     cv::namedWindow("frame");
     cv::namedWindow("foreground");
 
-    auto backgroundSubtractorPtr = cv::bgsegm::createBackgroundSubtractorGMG(10, 0.9);
+   // auto backgroundSubtractorPtr = cv::bgsegm::createBackgroundSubtractorGMG(10, 0.9);
+    auto backgroundSubtractorPtr = cv::bgsegm::createBackgroundSubtractorMOG();
+
 
     cv::VideoCapture capture(videoPath);
     if (!capture.isOpened()) {
@@ -56,7 +59,40 @@ int main() {
     Ipm ipm( cv::Size(width, height), cv::Size(width, height), inputPoints, outputPoints);
     */
 
-    std::string videoPath = "./Video_001.avi";
-    subtractBackground(videoPath, false);
+//    std::string videoPath = "./Video_001.avi";
+//    subtractBackground(videoPath, false);
+
+    cv::Size patternSize(6, 9);
+    std::string calibImagePath = "chess.jpg";
+    cv::Mat calibImage = cv::imread(calibImagePath, cv::IMREAD_GRAYSCALE);
+
+    cv::namedWindow("chess");
+    cv::imshow("chess", calibImage);
+
+    std::vector<cv::Point2f> corners;
+    bool isFound = cv::findChessboardCorners(calibImage, patternSize, corners, cv::CALIB_CB_ADAPTIVE_THRESH + cv::CALIB_CB_NORMALIZE_IMAGE + cv::CALIB_CB_FAST_CHECK);
+
+    std::cout << isFound << std::endl << corners << std::endl;
+
+    cv::waitKey(0);
+
+    std::vector<cv::Point2f> worldPoints;
+    for (int i = 0; i < patternSize.height; i++) {
+        for (int j = 0; j < patternSize.width; j++) {
+
+        }
+    }
+
+//    std::string intrinsicsPath = "./intrinsics.xml";
+//    cv::Mat cameraMatrix, distCoeffs;
+//    cv::FileStorage fs(intrinsicsPath, cv::FileStorage::READ);
+//    fs["MY_MAT_NAME_IN_THE_XML"] >> cameraMatrix;
+//    fs["MY_MAT_NAME_IN_THE_XML"] >> distCoeffs;
+//    fs.release();
+//
+//    cv::Mat rotationVector, translationVector;
+//    cv::solvePnP(worldPoints, corners, cameraMatrix, distCoeffs, rotationVector, translationVector, false, cv::SOLVEPNP_EPNP);
+
+    //cv::projectPoints()
     return 0;
 }
