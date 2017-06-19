710257c44fa81ff53b06dbfe4d2886a29b359ffc
dailydreamer <lixc12@outlook.com>
2017-06-19 11:44:42 +0800

    finish compute extrinsic

--- main.cpp
+++ main.cpp
@@ -7,14 +7,89 @@
 #include <opencv2/aruco.hpp>
 #include "ipm.h"
 
-bool readCameraParameters(std::string filename, cv::Mat &camMatrix, cv::Mat &distCoeffs) {
-    std::cout << filename << std::endl;
+bool readIntrinsic(std::string filename, cv::Mat& camMatrix, cv::Mat& distCoeffs) {
     cv::FileStorage fs(filename, cv::FileStorage::READ);
-    if(!fs.isOpened())
+    if(!fs.isOpened()) {
+        fs.release();
         return false;
+    }
     fs["camera_matrix"] >> camMatrix;
     fs["distortion_coefficients"] >> distCoeffs;
+    fs.release();
     return true;
+}
+
+bool writeExtrinsic(std::string filename, std::vector<cv::Vec3d> rvecs, std::vector<cv::Vec3d> tvecs) {
+    cv::FileStorage fs(filename, cv::FileStorage::WRITE);
+    if(!fs.isOpened()) {
+        fs.release();
+        return false;
+    }
+    assert(rvecs.size() == tvecs.size());
+    fs << "size" << static_cast<int>(rvecs.size());
+    for (int i = 0; i < rvecs.size(); ++i) {
+        fs << "rvec_" + std::to_string(i) << rvecs[i];
+        fs << "tvec_" + std::to_string(i) << tvecs[i];
+    }
+    fs.release();
+    return true;
+}
+
+bool readExtrinsic(std::string filename, std::vector<cv::Vec3d>& rvecs, std::vector<cv::Vec3d>& tvecs) {
+    cv::FileStorage fs(filename, cv::FileStorage::READ);
+    if(!fs.isOpened()) {
+        fs.release();
+        return false;
+    }
+    int size;
+    fs["size"] >> size;
+    cv::Vec3d tmpRvec, tmpTvec;
+    for (int i = 0; i < size; i++) {
+        fs["rvec_" + std::to_string(i)] >> tmpRvec;
+        fs["tvec_" + std::to_string(i)] >> tmpTvec;
+        rvecs.push_back(tmpRvec);
+        tvecs.push_back(tmpTvec);
+    }
+    fs.release();
+    return true;
+}
+
+void computeExtrinsic(std::string calibImagePath, float markerLength, const cv::Mat& cameraMatrix, const cv::Mat& distCoeffs) {
+    cv::Mat calibImage = cv::imread(calibImagePath);
+    //cv::resize(calibImage, calibImage, cv::Size(800, 600));
+
+    std::vector<int> markerIds;
+    std::vector<std::vector<cv::Point2f>> corners, rejected;
+    auto dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+    auto detectorParams = cv::aruco::DetectorParameters::create();
+    detectorParams->cornerRefinementMethod = cv::aruco::CORNER_REFINE_SUBPIX;
+    cv::aruco::detectMarkers(calibImage, dictionary, corners, markerIds, detectorParams, rejected);
+
+    std::cout <<"marker: " << markerIds.size() << " rejected: " << rejected.size() << std::endl;
+
+    std::vector<cv::Vec3d> rvecs, tvecs;
+    cv::aruco::estimatePoseSingleMarkers(corners, markerLength, cameraMatrix, distCoeffs, rvecs, tvecs);
+    bool writeSuccess = writeExtrinsic("./extrinsic.yml", rvecs, tvecs);
+    if (!writeSuccess) {
+        std::cout << "write extrinsic failed" << std::endl;
+    }
+
+//    cv::Mat resImage;
+//    calibImage.copyTo(resImage);
+
+//    cv::aruco::drawDetectedMarkers(resImage, corners, markerIds);
+//    cv::aruco::drawDetectedMarkers(resImage, rejected);
+
+//    std::cout << cameraMatrix << std::endl << distCoeffs << std::endl;
+
+//    std::cout << rvecs[0] << std::endl << tvecs[0] << std::endl;
+//    for(auto i = 0; i < markerIds.size(); ++i) {
+//        std::cout << i << std::endl;
+//        cv::aruco::drawAxis(resImage, cameraMatrix, distCoeffs, rvecs[i], tvecs[i], markerLength * 0.5f);
+//    }
+//
+//    cv::namedWindow("res", cv::WINDOW_NORMAL | cv::WINDOW_KEEPRATIO);
+//    cv::imshow("res", resImage);
 }
 
 void generateArucoMarker() {
@@ -121,36 +196,21 @@ int main() {
 
 //    generateArucoMarker();
 
-    std::string calibImagePath = "aruco7.png";
-    cv::Mat calibImage = cv::imread(calibImagePath);
-    //cv::resize(calibImage, calibImage, cv::Size(800, 600));
+    // compute extrinsic
+//    float markerLength = 0.14;    // 14cm
+//    cv::Mat cameraMatrix, distCoeffs;
+//    bool readIntrinsicSuccess = readIntrinsic("./intrinsic.yml", cameraMatrix, distCoeffs);
+//    if (!readIntrinsicSuccess) {
+//        std::cout << "read intrinsic failed" << std::endl;
+//    }
+//    computeExtrinsic("aruco.png", markerLength, cameraMatrix, distCoeffs);
 
-    std::vector<int> markerIds;
-    std::vector<std::vector<cv::Point2f>> corners, rejected;
-    auto dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
-    auto detectorParams = cv::aruco::DetectorParameters::create();
-    detectorParams->cornerRefinementMethod = cv::aruco::CORNER_REFINE_SUBPIX;
-    cv::aruco::detectMarkers(calibImage, dictionary, corners, markerIds, detectorParams, rejected);
-
-    cv::Mat resImage;
-    calibImage.copyTo(resImage);
-
-    std::cout <<"marker: " << markerIds.size() << " rejected: " << rejected.size() << std::endl;
-
-    cv::aruco::drawDetectedMarkers(resImage, corners, markerIds);
- //   cv::aruco::drawDetectedMarkers(resImage, rejected);
-    cv::namedWindow("res", cv::WINDOW_NORMAL | cv::WINDOW_KEEPRATIO);
-    cv::imshow("res", resImage);
-
-    float markerLength = 0.14;    // 14cm
     std::vector<cv::Vec3d> rvecs, tvecs;
-    cv::Mat cameraMatrix, distCoeffs;
-    readCameraParameters("./ost.yaml", cameraMatrix, distCoeffs);
-    cv::aruco::estimatePoseSingleMarkers(corners, markerLength, cameraMatrix, distCoeffs, rvecs, tvecs);
-    for(auto i = 0; i < markerIds.size(); ++i) {
-        cv::aruco::drawAxis(resImage, cameraMatrix, distCoeffs, rvecs[i], tvecs[i], markerLength * 0.5f);
+    bool readExtrinsicSuccess = readExtrinsic("./extrinsic.yml", rvecs, tvecs);
+    if (!readExtrinsicSuccess) {
+        std::cout << "read extrinsicc failed" << std::endl;
     }
-
+    std::cout << rvecs[0] << std::endl << tvecs[0] << std::endl;
     cv::waitKey(0);
 
     return 0;
