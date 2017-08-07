5e5fa242634ebfeba7adf668d08d85c7fe629751
dailydreamer <lixc12@outlook.com>
2017-08-07 18:59:32 +0800

    try a way intersect two vectors but doesn't work bechause points are type double and discrete sparse vales

--- main.cpp
+++ main.cpp
@@ -4,6 +4,25 @@
 #include <opencv2/bgsegm.hpp>
 #include "ipm.h"
 
+bool comparePoints(const cv::Point2d p1, const cv::Point2d p2)
+{
+  if (p1.x != p2.x) {
+    return p1.x < p2.x;
+  } else {
+    return p1.y < p2.y;
+  }
+}
+
+//  in c++ 11, vector has move operator
+std::vector<cv::Point2d> intersection(std::vector<cv::Point2d>& v1, std::vector<cv::Point2d>& v2)
+{
+    std::vector<cv::Point2d> res;
+    std::sort(v1.begin(), v1.end(), comparePoints);
+    std::sort(v2.begin(), v2.end(), comparePoints);
+    std::set_intersection(v1.begin(), v1.end(), v2.begin(), v2.end(), std::back_inserter(res), comparePoints);
+    return res;
+}
+
 void drawCircle( cv::Mat img, cv::Point center )
 {
     int thickness = -1;
@@ -36,31 +55,38 @@ int main() {
     auto backgroundSubtractorPtr = cv::bgsegm::createBackgroundSubtractorMOG();
 //    auto backgroundSubtractorPtr = cv::createBackgroundSubtractorMOG2(500, 16, true);
     
-    cv::Mat frame[cameraNum], foreground[cameraNum];
+    cv::Mat originFrame[cameraNum], foregroundFrame[cameraNum];
     cv::Mat birdViewFrame[cameraNum];
     std::vector<cv::Point> foregroundPoints[cameraNum];
     std::vector<cv::Point2d> birdViewForegroundPoints[cameraNum];
     // loop until press q or esc
     for (int key = 0; key != 'q' && key != 27; ) {
       for (int i = 0; i < cameraNum; i++) {
-	if (!capture[i].read(frame[i])) {
+	if (!capture[i].read(originFrame[i])) {
 	  std::cerr << "ERROR: Cannot read next frame of camera " << i << std::endl;
 	  exit(EXIT_FAILURE);
         }
-        backgroundSubtractorPtr -> apply(frame[i], foreground[i]);
-        cv::findNonZero(foreground[i], foregroundPoints[i]);
-        ipm[i].getIpmImage(frame[i], birdViewFrame[i]);
-//        ipm.getIpmPoints(foregroundPoints, birdViewForegroundPoints);
-        auto position = cv::mean(foregroundPoints[i]);
+        backgroundSubtractorPtr -> apply(originFrame[i], foregroundFrame[i]);
+        cv::findNonZero(foregroundFrame[i], foregroundPoints[i]);
+        ipm[i].getIpmImage(originFrame[i], birdViewFrame[i]);
+        ipm[i].getIpmPoints(foregroundPoints[i], birdViewForegroundPoints[i]);
+        auto position = cv::mean(birdViewForegroundPoints[i]);
         auto x = position[0], y = position[1];
-
-//        std::cout << foregroundPoints << std::endl << x << " " << y << std::endl;
-
-        drawCircle(foreground[i], cv::Point(x,y));
-        cv::imshow(windowLabels[i]+"origin", frame[i]);
+       drawCircle(birdViewFrame[i], cv::Point(x,y));
+        cv::imshow(windowLabels[i]+"origin", originFrame[i]);
         cv::imshow(windowLabels[i]+"birdview", birdViewFrame[i]);
       }
       
+      // TODO can be change to reduce format, so that can handle any number of cameras
+//      auto minimalBirdViewForegroundPoints = intersection(birdViewForegroundPoints[0], birdViewForegroundPoints[1]);
+//      auto position = cv::mean(minimalBirdViewForegroundPoints);
+//      auto x = position[0], y = position[1];
+//      std::cout << "position: " <<  x << " " << y << std::endl;
+//      drawCircle(birdViewFrame[0], cv::Point(x,y));
+//      cv::imshow(windowLabels[0]+"birdview",  birdViewFrame[0]);
+//      drawCircle(birdViewFrame[1], cv::Point(x,y));
+//      cv::imshow(windowLabels[1]+"birdview",  birdViewFrame[1]);
+  
       if (debug) {
 	key = cv::waitKey(500);
       } else {
